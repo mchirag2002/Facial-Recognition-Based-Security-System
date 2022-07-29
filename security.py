7e0735ae4e85443d51b82a8ec07c05204636603a
Chirag Mathur <mchirag2002@gmail.com>
2022-07-29 12:54:51 +0530

    file_handling

--- security.py
+++ security.py
@@ -7,30 +7,42 @@ import time
 import os
 
 board = Arduino('COM3')
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier(
+    'cascades/data/haarcascade_frontalface_alt2.xml')
 video_capture = cv2.VideoCapture(0)
 
 
-elon_image = face_recognition.load_image_file("elon.jpg")
-elon_face_encoding = face_recognition.face_encodings(elon_image)[0]
+path = 'E:\PROJECTS\FACIAL RECOGNITION\Database'
+peopleImg = []
+known_face_names = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curimg = cv2.imread(f'{path}/{cl}')  # politicians image/Amit_Shah.jpg
+    peopleImg.append(curimg)
+    # Here 0 depicts the first part of image name
+    known_face_names.append(os.path.splitext(cl)[0])
+# print(peopleName)
 
 
-chirag_image = face_recognition.load_image_file("chirag.png")
-chirag_face_encoding = face_recognition.face_encodings(chirag_image)[0]
+def resize(img, size):
+    width = int(img.shape[1]*size)
+    height = int(img.shape[0] * size)
+    dimension = (width, height)
+    return cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
 
-keanu_image = face_recognition.load_image_file("keanu.jpg")
-keanu_face_encoding = face_recognition.face_encodings(keanu_image)[0]
 
-known_face_encodings = [
-    elon_face_encoding,
-    #chirag_face_encoding,
-    keanu_face_encoding
-]
-known_face_names = [
-    "Elon Musk",
-    #"Chirag Mathur",
-    "Keanu Reeves"
-]
+def findEncoding(images):
+    imgEncodings = []
+    for img in images:
+        img = resize(img, 0.50)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encodeimg = face_recognition.face_encodings(img)[0]
+        imgEncodings.append(encodeimg)
+    return imgEncodings
+
+
+known_face_encodings = findEncoding(peopleImg)
 
 
 face_locations = []
@@ -40,40 +52,41 @@ process_this_frame = True
 
 
 while True:
-    
+
     ret, frame = video_capture.read()
 
-   
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-   
     rgb_small_frame = small_frame[:, :, ::-1]
 
-   
     if process_this_frame:
-        
+
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_encodings = face_recognition.face_encodings(
+            rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
-          
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+            matches = face_recognition.compare_faces(
+                known_face_encodings, face_encoding)
             name = "Unknown"
 
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            face_distances = face_recognition.face_distance(
+                known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
-            if name=="Unknown":
+            if name == "Unknown":
                 board.digital[13].write(1)
                 time.sleep(2)
                 board.digital[13].write(0)
                 time.sleep(1)
-                gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray,scaleFactor=1.5, minNeighbors=5)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(
+                    gray, scaleFactor=1.5, minNeighbors=5)
                 for (x, y, w, h) in faces:
                     roi_gray = gray[y:y+h, x:x+h]
                     img_item = "unknown person.png"
@@ -81,27 +94,23 @@ while True:
 
     process_this_frame = not process_this_frame
 
-
-   
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-       
+
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-      
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-        
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 35),
+                      (right, bottom), (0, 255, 0), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 0, 0), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6),
+                    font, 1.0, (0, 0, 0), 1)
 
-   
     cv2.imshow('Video', frame)
 
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+board = Arduino('COM3')
